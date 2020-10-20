d02eeb149352be95009996cd31e453b9887abc5e
Allie <williamson.alisonj@gmail.com>
2020-10-19 22:50:06 -0600

    added require morgan to server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,8 +8,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
+app.use(morgan('combined'))
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
