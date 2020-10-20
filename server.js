8e42994f9bd28f7357809e757bd4dec5865227e9
Allie <williamson.alisonj@gmail.com>
2020-10-19 23:45:34 -0600

    deploying

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+
 });
 
 // routes
